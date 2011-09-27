3c83a1d85ecef6254ee3e78c2aa97deb50a717ca
Ryoichi KATO <ryo1kato@example.com>
2011-09-27 15:08:12 +0900

    minor change for my workstation

--- edit.sh
+++ edit.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
 : ${EDITOR:=vim}
-case `uname -o` in
-    Cygwin) GUI_EDITOR=cygwin_editor;;
-         *) : ${GUI_EDITOR:=gvim};;
-esac
+
+journal_dir="$HOME/usr/var/journal"
+#gtdnextday_hs="$HOME/src/gtdnextday/gtdnextday.hs"  # for Cygin/runghc
+#gtdnextday_exe="$HOME/src/gtdnextday/gtdnextday"  # for Linux/ghc precompiled
+gtdnextday_exe="$HOME/src/gtdnextday/gtdnextday"
+
+conf_use_gui () { false; }
 
 cygwin_editor () {
     export HOME="$HOMEPATH"
     /cygdrive/c/Programs/vim72/gvim.exe "$(cygpath -w "$1")"
 }
 
-journal_dir=$HOME/Dropbox/GTD/journal
+gtdnextday ()
+{
+    ## for Cygwin/runghc
+    # gtdnextday_hs="`cygpath -m $gtdnextday_hs`"
+    # runghc "$gtdnextday_hs" "$(cygpath -w "$1")" "$(cygpath -w "$2")"
+
+    "$HOME/Software/gtdnextday/gtdnextday" "$@"
+}
+
+#############################################################
+search_back_days_max=100
+
 todays_journal="$journal_dir/`date -I -d '2 hours ago'`".txt
+
+case `uname -o` in
+    Cygwin) GUI_EDITOR=cygwin_editor;;
+         *) : ${GUI_EDITOR:=gvim};;
+esac
 
 MSG () { echo "$@"; }
 
@@ -36,9 +55,11 @@ gui_running ()
     esac
 }
 
+use_gui () { gui_running && conf_use_gui; }
+
 start_editor ()
 {
-    if gui_running
+    if use_gui
     then
         "${GUI_EDITOR}" "$1"
     else
@@ -54,27 +75,14 @@ search_old_journal ()
     do
         file="$journal_dir/`date -I -d "$i day ago"`.txt"
         (( i++ ))
+        if [ $i -ge $search_back_days_max ]
+        then
+            return 1
+        fi
     done
     echo "$file"
 }
 
-gtdnextday ()
-{
-    case `uname -o` in
-        Cygwin)
-            gtdnextday_hs="$journal_dir/../../Software/gtdnextday/gtdnextday.hs"
-            gtdnextday_hs="`cygpath -m $gtdnextday_hs`"
-            runghc "$gtdnextday_hs" "$(cygpath -w "$1")" "$(cygpath -w "$2")"
-            ;;
-        *)
-            gtdnextday_exe="$journal_dir/../gtdnextday"
-            if [ ! -x "$gtdnextday_exe" ]; then
-                chmod +x "$gtdnextday_exe"
-            fi
-            "$gtdnextday_exe" "$@"
-            ;;
-    esac
-}
 
 if ! check_if_date_is_gnudate
 then
@@ -95,10 +103,18 @@ then
         gtdnextday "$oldfile" "$todays_journal" ||
             DIE "gtdnextday failed for $oldfile"
     else
-        DIE "No current or old journal found"
+        MSG "No previous journal for these $search_back_days_max days"
+        MSG "Start with empty file"
+        printf "%s\n\n\nvim: ft=todo\n" "`date -I`" > "$todays_journal"
     fi
 fi
 MSG "Start editing $todays_journal"
 ln -sf "$todays_journal" "$journal_dir/today.txt"
-start_editor "$todays_journal" &
+
+if use_gui 
+then
+    start_editor "$todays_journal" &
+else
+    start_editor "$todays_journal"
+fi
 
